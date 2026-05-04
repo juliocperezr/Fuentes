dec8f5f9a6cef8296df795eaab1bc6f4a5abcb04
Julio César Pérez <juliodelarosa.94@gmail.com>
2026-05-03 21:25:24 -0600

    11subida

---
+++ script.js
@@ -0,0 +1,312 @@
+const recursos = [
+
+{
+titulo:"Adultocentrismo",
+contenido:`
+<h2>Superando el adultocentrismo</h2>
+<p><strong>Rodríguez Tramolao, S. (2013). Santiago de Chile: Fondo de las Naciones Unidas para la Infancia (UNICEF).</strong></p>
+
+<p>El cuadernillo tiene como objetivo promover que las personas adultas se conviertan en verdaderas aliadas de las y los adolescentes, con el propósito de fortalecer el ejercicio de sus derechos a la expresión y la participación. 
+</p>
+
+<p>Se centra en el análisis del proceso mediante el cual opera el adultocentrismo en las personas adultas y ofrece orientaciones sobre cómo podemos superarlo. </p>
+
+<p>Proporciona herramientas que permiten a las personas adultas acompañar el proceso de toma de decisiones de las y los adolescentes, sin invalidar su voz ni anular su personalidad. </p>
+
+<a href="https://www.imageneseducativas.com/wp-content/uploads/2019/02/Superando-el-Adultocentrismo.pdf" target="_blank" class="btn">Consultar</a>
+`
+},
+
+{
+titulo:"Convivencia con enfoque formativo",
+contenido:`
+<h2>Construyendo acuerdos de convivencia con enfoque formativo. Propuestas y sugerencias prácticas para los reglamentos internos de las comunidades educativas</h2>
+<p><strong>UNICEF. (2022). Santiago de Chile: UNICEF.</strong></p>
+
+<p>Esta cartilla ofrece propuestas para avanzar en la formulación de reglamentos internos con un enfoque formativo, a fin de que se alineen con el enfoque de derechos.
+</p>
+
+<p>Cada propuesta se acompaña de ejemplos concretos que ilustran posibles formas de implementación en esta línea. 
+</p>
+
+<a href="https://www.unicef.org/chile/media/6991/file/construyendo%20acuerdos%20.pdf" target="_blank" class="btn">Consultar</a>
+`
+},
+
+{
+titulo:"Reglamentos en escuelas",
+contenido:`
+<h2>Análisis de reglamentos en escuelas de Educación Básica en México. Disciplina, autoridad y
+ejercicio del poder
+</h2>
+<p><strong>Landeros, L. & Chávez, M. (2015). Ponencia presentada en el XIII Congreso Nacional de Investigación Educativa, Chihuahua, México.</strong></p>
+
+<p>La ponencia presenta los resultados de un estudio en el que se analizaron más de seiscientos reglamentos escolares y de aula, provenientes de escuelas de educación básica en tres entidades de la República Mexicana. Se comparten algunos hallazgos relacionados con los significados que se atribuyen a la disciplina y la autoridad a través de las normas. 
+</p>
+
+<p>Asimismo, en lo que respecta a la autoridad, se identifican elementos que evidencian la
+unilateralidad presente en la mayoría de los reglamentos.
+</p>
+
+<a href="https://www.comie.org.mx/congreso/memoriaelectronica/v13/doc/0419.pdf" target="_blank" class="btn">Consultar</a>
+`
+},
+
+{
+titulo:"Derechos de la niñez",
+contenido:`
+<h2>El enfoque basado en los derechos de la niñez</h2>
+<p><strong>UNICEF. (2022). Serie de formación sobre el enfoque basado en los derechos de la niñez.
+Módulo 1. Santiago de Chile: UNICEF.
+</strong></p>
+
+<p>Este documento aborda el Enfoque Basado en los Derechos de la Niñez, su origen, características, evolución e implicaciones.
+</p>
+
+<p>Se destaca el papel de las personas funcionarias públicas como representantes del Estado y principales garantes de los derechos de niñas, niños y adolescentes.
+</p>
+
+<a href="https://www.unicef.org/chile/media/7021/file/mod%201%20enfoque%20de%20derechos.pdf" target="_blank" class="btn">Consultar</a>
+`
+},
+
+{
+titulo:"Derecho a la participación",
+contenido:`
+<h2>Derecho a la participación de niñas, niños y adolescentes</h2>
+<p><strong>UNICEF. (2022). Serie de formación sobre el enfoque basado en los derechos de la niñez.
+Módulo 4. Santiago de Chile: UNICEF.
+</strong></p>
+
+<p>El documento aborda la participación de niñas, niñas y adolescentes como un derecho fundamental y un principio general establecido en la Convención sobre los Derechos del Niño (CDN). Este principio es esencial para la realización efectiva de todos los demás derechos de la infancia, y resalta la importancia de escuchar activamente sus voces y garantizar su incidencia en los procesos que afectan sus vidas.
+</p>
+
+
+<a href="https://www.unicef.org/chile/media/7031/file/Mod%204%20derecho%20participacion.pdf" target="_blank" class="btn">Consultar</a>
+`
+},
+
+{
+titulo:"Participación infantil y adolescente",
+contenido:`
+<h2>La escuela como espacio de participación infantil y adolescente para la formación ciudadana</h2>
+<p><strong>Hernández Barrientos, W. Y., Paredes Ramírez, L. E., Rodríguez Cordero, P., Rodríguez Huacuz, M. D., Góngora Martínez, A. L., & Martínez Hernández, M. L. (2020). Subsecretaría de Educación Básica. México: SEP.</strong></p>
+
+<p>Este documento está dirigido a la comunidad educativa de escuelas primarias y secundarias, y tiene como objetivo ofrecer orientaciones y estrategias en torno a dos mecanismos clave de participación escolar: las asambleas y los proyectos escolares. 
+</p>
+
+<p>Las estrategias propuestas buscan fomentar la convivencia democrática, fortalecer los vínculos sociales y promover el sentido de pertenencia y respeto dentro de la comunidad educativa.
+
+</p>
+
+<a href="https://www.educacionbc.edu.mx/departamentos/partsocial/archivos2020/convivencia%20escolar/La_escuela_espacio_de_participacion.pdf" target="_blank" class="btn">Consultar</a>
+`
+},
+
+{
+titulo:"Rompiendo ciclos",
+contenido:`
+<h2>Rompiendo ciclos: Prevención y abordaje de la violencia en el sector de la educación</h2>
+<p><strong>UNESCO. (2025, 11 de septiembre). Tercera sesión de la serie sobre prevención y abordaje de violencia de género en entornos educativos [Seminario web].</strong></p>
+
+
+<p>Define la violencia de género y la diferencia de otras formas de violencia, como la ejercida contra las mujeres y la dirigida hacia personas de la diversidad sexual.
+Destaca la necesidad de analizar la masculinidad tradicional y su vínculo con la violencia de género y el maltrato en el ámbito familiar.
+</p>
+
+<p>Se reconoce que el espacio escolar, como lugar de socialización, puede reproducir la violencia de género; sin embargo, también se le identifica como un entorno con potencial para transformar esa realidad mediante el desarrollo del pensamiento crítico. 
+Se abordan los principales retos en la prevención de la violencia de género 
+</p>
+
+<a href="https://www.youtube.com/watch?v=UaYi8bg8Puk" target="_blank" class="btn">Ver</a>
+`
+},
+
+{
+titulo:"Maltrato en niños y niñas",
+contenido:`
+<h2>¿Qué efectos tiene el maltrato en niños y niñas? Escucha las recomendaciones de la experta 
+de UNICEF
+</h2>
+<p><strong>UNICEF Chile. (2023, 10 de agosto). [Archivo de video]. YouTube.</strong></p>
+
+
+<p>A partir de la situación de violencia que afecta a la niñez y la adolescencia en Chile, este video corto destaca las huellas imborrables que deja cualquier tipo de maltrato en la vida de niñas, niños y adolescentes, así como su impacto en sus procesos de desarrollo. </p>
+
+<a href="https://youtube.com/watch?v=kP3DAnOLHv0" target="_blank" class="btn">Ver</a>
+`
+},
+
+{
+titulo:"Violencia en la crianza",
+contenido:`
+<h2>¿Cuáles son las consecuencias de la violencia en la crianza?</h2>
+<p><strong>UNICEF. (2020). Uruguay: UNICEF.</strong></p>
+
+<p>El artículo parte de la premisa de que la violencia siempre deja huellas en niñas, niños y adolescentes. En este contexto, analiza cómo los distintos tipos de violencia los afectan y expone algunas de las consecuencias físicas, psicológicas y sociales más frecuentes del castigo físico y el maltrato psicológico.</p>
+
+<a href="https://www.unicef.org/uruguay/crianza/etapa-escolar/cuales-son-las-consecuencias-delaviolencia-en-la-crianza" target="_blank" class="btn">Consultar</a>
+`
+},
+
+{
+titulo:"Decálogo de acción",
+contenido:`
+<h2>Presenta DIF Nacional Decálogo de acción ante el maltrato infantil</h2>
+<p><strong>Sistema Nacional DIF. (2025, febrero 22). [Comunicado de prensa].</strong></p>
+
+<p>El Sistema Nacional para el Desarrollo Integral de la Familia, a través de la Procuraduría Federal de Protección de Niñas, Niños y Adolescentes, presenta una serie de recomendaciones reunidas en el Decálogo de acción ante el maltrato infantil. </p>
+
+<a href="https://www.gob.mx/difnacional/articulos/nuevo-comunicado" target="_blank" class="btn">Consultar</a>
+`
+},
+
+{
+titulo:"Usar 911",
+contenido:`
+<h2>¿Cómo enseñar a niñas, niños y adolescentes a usar el 911?</h2>
+<p><strong>Antillón, S. (2023, diciembre). UNICEF México.</strong></p>
+
+<p>El artículo se centra en cómo enseñar a niñas, niños y adolescentes el uso y el cuidado del servicio de emergencia, con el fin de protegerse a sí mismas, a sí mismos y a las demás personas.
+
+</p>
+
+<a href="https://www.unicef.org/mexico/historias/%C2%BFc%C3%B3mo-ense%C3%B1arni%C3%B1asni%C3%B1os-y-adolescentes-usar-el-911" target="_blank" class="btn">Consultar</a>
+`
+},
+
+{
+titulo:"Infografía 911",
+contenido:`
+<h2>Hoja de ayuda. En caso de emergencia llama al 911</h2>
+<p><strong>Secretaría de Gobernación, Secretaría de Seguridad y Protección Ciudadana, Secretariado
+Ejecutivo, SIPINNA & UNICEF. (s. f.). [Infografía].
+</strong></p>
+
+<p>Infografía que explica el proceso para solicitar ayuda de la policía, una ambulancia o el cuerpo de bomberos a través del 911.</p>
+
+<a href="https://www.unicef.org/mexico/media/7721/file/Hoja%20de%20ayuda%20911.pdf" target="_blank" class="btn">Consultar</a>
+`
+},
+
+{
+titulo:"Herramientas",
+contenido:`
+<h2>Herramientas para madres, padres y personas cuidadoras sobre el uso de la línea de
+emergencia 9-1-1 por niñas, niños y adolescentes
+</h2>
+<p><strong>Secretaría de Gobernación, Secretaría de Seguridad y Protección Ciudadana, Secretariado
+Ejecutivo, SIPINNA & UNICEF. (s. f.). [Infografía].
+</strong></p>
+<p>Dirigido a madres, padres de familia y personas cuidadoras, este recurso explica la importancia de desarrollar criterios para identificar cuándo es apropiado utilizar la línea 9-1-1, así como el papel activo que pueden desempeñar en la atención de una emergencia que requiere su intervención inmediata.
+</p>
+
+<a href="https://www.unicef.org/mexico/media/7726/file/Herramientas%20para%20madres,%20padres%20y%20personas%20cuidadoras.pdf" target="_blank" class="btn">Consultar</a>
+`
+},
+
+{
+titulo:"Kit de herramientas escolares",
+contenido:`
+<h2>Kit de herramientas para la convivencia escolar. Material pedagógico para prevenir el Maltrato Infanti</h2>
+<p><strong>Portal Colombia Aprende. (s. f.). Ministerio de Educación Nacional.
+</strong></p>
+<p>Este material pedagógico de prevención del maltrato infantil está dirigido a niñas y niños, con el objetivo de que se reconozcan a sí mismas y a sí mismos como personas fundamentales en la sociedad. Se les presenta como sujetos de derecho con especial protección, cuyo interés superior debe ser garantizado por el Estado, la sociedad y la familia.</p>
+
+<a href="https://www.colombiaaprende.edu.co/recurso-coleccion/material-pedagogico-paraprevenirel-maltrato-infantil" target="_blank" class="btn">Consultar</a>
+`
+},
+
+{
+titulo:"Fin a la violencia",
+contenido:`
+<h2>Consejos para poner fin a la violencia contra los niños y niñas</h2>
+<p><strong>UNICEF. (2023).
+</strong></p>
+<p>Incluye recursos y herramientas dirigidos a madres, padres de familia y otras personas cuidadoras a cargo de niñas, niños y adolescentes, con el propósito de fomentar la reflexión y la prevención de la violencia, así como contenidos que les ayuden a gestionar mejor sus emociones.
+</p>
+
+<a href="https://www.unicef.org/lac/crianza/seguridad-proteccion/consejos-para-poner-fin-laviolencia-contra-ninos-ninas" target="_blank" class="btn">Consultar</a>
+`
+},
+
+{
+titulo:"Relaciones respetuosas",
+contenido:`
+<h2>Construyendo relaciones respetuosas en las familias</h2>
+<p><strong>Sistema para el Desarrollo Integral de la Familia de la Ciudad de México (DIF-CDMX). (2017).
+</strong></p>
+<p>Folleto de carácter informativo dirigido a las familias sobre prácticas de crianza con perspectiva de género, la construcción de relaciones entre las y los integrantes de la familia basadas en el respeto y el cuidado, y la reflexión sobre los roles de género, priorizando siempre el bienestar de niñas y niños.
+</p>
+
+<a href="https://www.dif.cdmx.gob.mx/storage/app/media/uploadedfiles/folletoParentalidadPos_V1.5_paraImpresion.pdf" target="_blank" class="btn">Consultar</a>
+`
+},
+
+{
+titulo:"Seguridad",
+contenido:`
+<h2>Batería de la seguridad</h2>
+<p><strong>Secretaría de Educación Pública (2025). [Infografía].
+</strong></p>
+<p>Infografía dirigida a niñas, niños y adolescentes para que conozcan e identifiquen acciones de maltrato que dañan su bienestar.</p>
+
+<a href="https://escuelalibredeviolencia.sep.gob.mx/storage/recursos/Insumos%20jornada/wXGL8cuXEt-Bateria_de_seguridad.pdf" target="_blank" class="btn">Consultar</a>
+`
+},
+
+{
+titulo:"Mi voz",
+contenido:`
+<h2>Mi voz tiene poder</h2>
+<p><strong>Secretaría de Educación Pública, Subsecretaría de Educación Básica & Dirección General de Gestión Educativa y Enfoque Territorial. (2025). [Infografía].
+</strong></p>
+<p>Infografía dirigida a niñas, niños y adolescentes que muestra acciones para identificar y actuar ante una situación de maltrato.</p>
+
+<a href="https://escuelalibredeviolencia.sep.gob.mx/storage/recursos/Insumos%20jornada/T8QtCVMrM3-Mi%20voz%20tiene%20poder.pdf" target="_blank" class="btn">Consultar</a>
+`
+},
+
+{
+titulo:"Cuidar",
+contenido:`
+<h2>Cuidar también es escuchar</h2>
+<p><strong>Secretaría de Educación Pública, Subsecretaría de Educación Básica & Dirección General de Gestión Educativa y Enfoque Territorial. (2025). [Infografía].
+</strong></p>
+<p>Infografía dirigida a madres, padres de familia, personas tutoras y a toda la comunidad educativa, que ofrece información clave para reconocer y actuar ante el maltrato hacia niñas, niños y adolescentes.
+</p>
+
+<a href="https://escuelalibredeviolencia.sep.gob.mx/storage/recursos/Insumos%20jornada/1PhUG9qSKf-Cuidar%20tambien%20es%20escuchar.pdf" target="_blank" class="btn">Consultar</a>
+`
+}
+
+];
+
+// GENERAR MENU
+const menu = document.getElementById("menu");
+
+recursos.forEach((r)=>{
+    const item=document.createElement("div");
+    item.className="menu-item";
+    item.textContent=r.titulo;
+
+    item.onclick=()=>{
+        document.querySelectorAll(".menu-item").forEach(i=>i.classList.remove("active"));
+        item.classList.add("active");
+
+        document.getElementById("visor").innerHTML=`
+        <div class="card">
+            ${r.contenido}
+        </div>`;
+    };
+
+    menu.appendChild(item);
+});
+
+// BUSCADOR
+document.getElementById("buscador").addEventListener("input",function(){
+    const val=this.value.toLowerCase();
+    document.querySelectorAll(".menu-item").forEach(i=>{
+        i.style.display=i.textContent.toLowerCase().includes(val)?"block":"none";
+    });
+});
